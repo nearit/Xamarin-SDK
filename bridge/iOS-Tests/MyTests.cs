44c8cb619d2de848cc46c3a683f634c6fa7fa8fa
Cattaneo Stefano <stefano@nearit.com>
2018-04-12 16:14:12 +0200

    android and ios tests to check json conversion

--- bridge/iOS-Tests/MyTests.cs
+++ bridge/iOS-Tests/MyTests.cs
@@ -55,5 +55,58 @@ namespace iOSTests
             Assert.True(value is string);
             Assert.True(XFeed.TrackingInfo.extras["key"].Equals("value"));
         }
+
+        [Test]
+        public void FromCustomJsonNativeToCommon() {
+          
+            NSMutableDictionary<NSString, NSObject> nativeDic = new NSMutableDictionary<NSString, NSObject>();
+            nativeDic.Add(new NSString("nome"), new NSString("stefano"));
+            nativeDic.Add(new NSString("numero"), new NSNumber(4.6));
+
+            NSNumber number3 = new NSNumber(3.0);
+            NSNumber number4 = new NSNumber(4.0);
+            NSArray array = NSArray.FromObjects(number3, number4);
+            nativeDic.Add(new NSString("lista"), array);
+
+            NSMutableDictionary<NSString, NSObject> innerNativeDic = new NSMutableDictionary<NSString, NSObject>();
+            innerNativeDic.Add(new NSString("nome"), new NSString("martin"));
+            innerNativeDic.Add(new NSString("cognome"), new NSString("scorsese"));
+            NSDictionary<NSString, NSObject> innerDic = new NSDictionary<NSString, NSObject>(innerNativeDic.Keys, innerNativeDic.Values);
+            nativeDic.Add(new NSString("inner_object"), innerDic);
+
+            NSDictionary<NSString, NSObject> dic = new NSDictionary<NSString, NSObject>(nativeDic.Keys, nativeDic.Values);
+
+            NITCustomJSON nativeCUstomJson = new NITCustomJSON();
+            nativeCUstomJson.Content = dic;
+            nativeCUstomJson.NotificationMessage = "messaggio";
+            XCCustomJSONNotification jSONNotification = AdapterCustom.GetCommonType(nativeCUstomJson);
+
+            Assert.True(jSONNotification.Content is Dictionary<string, object>);
+
+            jSONNotification.Content.TryGetValue("nome", out object stringValue);
+            Assert.True(stringValue is string);
+            Assert.True(stringValue.Equals("stefano"));
+
+            jSONNotification.Content.TryGetValue("numero", out object doubleValue);
+            Assert.True(doubleValue is double);
+            Assert.True(doubleValue.Equals(4.6));
+
+            jSONNotification.Content.TryGetValue("lista", out object listValue);
+            Assert.True(listValue is List<object>);
+            Assert.True((listValue as List<object>)[0] is double);
+            Assert.True((double)(listValue as List<object>)[0] == 3.0);
+            Assert.True((listValue as List<object>)[1] is double);
+            Assert.True((double)(listValue as List<object>)[1] == 4.0);
+
+            jSONNotification.Content.TryGetValue("inner_object", out object innerObj);
+            Assert.True(innerObj is Dictionary<string, object>);
+            Dictionary<string, object> castedDic = innerObj as Dictionary<string, object>;
+            castedDic.TryGetValue("nome", out object name);
+            Assert.True(name is string);
+            Assert.True(((string)name).Equals("martin"));
+            castedDic.TryGetValue("cognome", out object cognome);
+            Assert.True(cognome is string);
+            Assert.True(((string)cognome).Equals("scorsese"));
+        }
     }
 }
